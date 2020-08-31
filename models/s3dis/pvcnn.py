240965e1cef7eb4aca408f2f1671ce56e662a983
YanXu <2801500185@qq.com>
2020-08-31 20:39:24 +0800

    comment

--- models/s3dis/pvcnn.py
+++ models/s3dis/pvcnn.py
@@ -7,12 +7,13 @@ __all__ = ['PVCNN']
 
 
 class PVCNN(nn.Module):
+    # pointnet blcoks : out_channels, num_blocks, voxel_resolution
     blocks = ((64, 1, 32), (64, 2, 16), (128, 1, 16), (1024, 1, None))
 
     def __init__(self, num_classes, extra_feature_channels=6, width_multiplier=1, voxel_resolution_multiplier=1):
         super().__init__()
         self.in_channels = extra_feature_channels + 3
-
+        # the arrangement of pytoch model
         layers, channels_point, concat_channels_point = create_pointnet_components(
             blocks=self.blocks, in_channels=self.in_channels, with_se=False,
             width_multiplier=width_multiplier, voxel_resolution_multiplier=voxel_resolution_multiplier
@@ -40,6 +41,7 @@ class PVCNN(nn.Module):
         for i in range(len(self.point_features)):
             inputs, _ = self.point_features[i]((inputs, coords))
             out_features_list.append(inputs)
+            
         # inputs: num_batches * 1024 * num_points -> num_batches * 1024 -> num_batches * 128
         inputs = self.cloud_features(inputs.max(dim=-1, keepdim=False).values)
         out_features_list.append(inputs.unsqueeze(-1).repeat([1, 1, coords.size(-1)]))
